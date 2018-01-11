daaebef6b977855f757cfaa16bea75db5d9e24e7
Minh930906 <minh930906@gmail.com>
2018-01-11 18:33:36 +0100

    Initialising the CNN

--- CNN/miki_cnn.py
+++ CNN/miki_cnn.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
+# Part 1 - Building the CNN
 
+#Importing the Keras libraries and Packages
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+#Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
