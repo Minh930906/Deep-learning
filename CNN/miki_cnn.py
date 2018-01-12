5cd348b9a55f2955942b352e9d58d4ae49cf5ac2
Minh930906 <minh930906@gmail.com>
2018-01-12 23:14:04 +0100

    image fitting

--- CNN/miki_cnn.py
+++ CNN/miki_cnn.py
@@ -29,3 +29,28 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))#output node
 classifier.compile(optimizer = 'adam',loss = 'binary_crossentropy', metrics =['accuracy'] )#binary because we only have binary outcome.If we had more than 2 outcome then we will choose categorical cross entropy
 
 # Step 2 Fit our CNN to our images
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,#rescale pixel values between 0 and 1
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size=(64, 64),#target size is the size of the images that is  in CNN model
+                                                 batch_size=32,
+                                                 class_mode='binary')#your independant variable has more than 2 categories
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+classifier.fit_generator(training_set,#first argument-->training set.   model.fit_generator we rename to classifier cuz our CNN model name is classifier
+                         steps_per_epoch=8000,#number of images we have in our training_set
+                         epochs=25,#number of epochs who wanna choose to train our CNN 
+                         validation_data=test_set,#that correspond to the testset on which we want to evaluate the performance of our CNN 
+                         validation_steps=2000)#correspond to number of images in our testset
